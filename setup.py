34963331b56a1acb70cec8da5457ec41040cbeba
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2026-02-26 21:02:06 +0100

    Add entrypoints and add to __init__

--- setup.py
+++ setup.py
@@ -89,5 +89,13 @@ setup(
         "aggregation": ["tsam"],
         "geometry": ["shapely", "scipy", "pyproj", "geojson", "pyshp"],
     },
-    entry_points={"console_scripts": ["ota = oemof.datapackage.cli:main"]},
+    entry_points={
+        "console_scripts": [
+            "ota = oemof.datapackage.cli:main",
+            "dp_to_json = oemof.datapackage.datapackage.convert_to_json:main",
+            "json_to_dp = oemof.datapackage.datapackage.convert_from_json:main",
+        ],
+
+
+    },
 )
--- src/oemof/datapackage/datapackage/__init__.py
+++ src/oemof/datapackage/datapackage/__init__.py
@@ -5,6 +5,9 @@ from . import building  # noqa F401
 from .reading import deserialize_constraints
 from .reading import deserialize_energy_system
 
+from .convert_from_json import rebuild_dp_from_json
+from .convert_to_json import export_dp_to_json
+
 EnergySystem.from_datapackage = classmethod(deserialize_energy_system)
 
 Model.add_constraints_from_datapackage = deserialize_constraints
